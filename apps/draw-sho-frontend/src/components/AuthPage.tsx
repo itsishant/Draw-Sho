8368e3fca7896c86639fe1e1c21f33948356c182
itsishant <modakmania@gmail.com>
2025-09-29 23:48:44 +0530

    add signin

---
+++ apps/draw-sho-frontend/src/components/AuthPage.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function AuthPage({isSignin}: {
+    isSigin: boolean
+}) {
+    return (
+        <>
+        <div>
+            hello
+        </div>
+        </>
+    )
+}
